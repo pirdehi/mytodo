7eacdbd42db976b012a92c8cb632b445677ab201
omid pirdehi <omidpirdehi@gmail.com>
2022-09-20 16:08:48 +0430

    added react icon - todo jsx

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
+import {AiOutlinePlus} from 'react-icons/ai'
+import Todo from './Todo'
 
+const style = {
+  bg:`h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#1CB5E0]`
+}
 
 function App() {
+  const [todos, setTodos] = useState(['Learn React', 'mio mio'])
   return (
-    <div >
+    <div className={style.bg}>
+      <div className={style.container}>
+        <h3 className={style.heading}>Todo App</h3>
+        <form className={style.form}>
+          <input className={style.input} type="text" placeholder="add to"></input>
+          <button className={style.button}><AiOutlinePlus size={30} /></button>
+        </form>
+        <uL>
+          <Todo />
+        </uL>
+
+      </div>
 
     </div>
   );
